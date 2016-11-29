96469589d63c6b384f6facf0e9ec995de11aa511
konstant2500 <konstant2500@gmail.com>
2016-11-29 10:49:30 -0800

    Initial commit

---
+++ Lesson_input.py
@@ -0,0 +1,5 @@
+lastName = input("Enter your last name")
+firstName = input("Enter your first name")
+colorFavorite = input("Enter your favorite color")
+
+print("To verify, your last name os %s, your first name is %s, and your favorite color is %s."  %(lastName, firstName, colorFavorite))
